1d1ae9b1c81b7be8130516fb959765d502857945
澤なつみ <n2natsu1218@gmail.com>
2016-12-14 17:45:26 +0900

    edit top#index / events#index

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,18 +1,11 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
   end
 
   def new
   end
 
   def create
-    Event.create(event_params)
-  end
-
-  private
-  def events_params
-    params.permit(:title, :time, :place, :detail)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get   '/'  => 'top#index'
-  get   'events'     => 'events#new'
-  get   'events/new' => 'events#new'
-  post  'events'     => 'events#create'
-
+  get 'events'=> 'events#index'
+  root :to => 'top#index'
 end

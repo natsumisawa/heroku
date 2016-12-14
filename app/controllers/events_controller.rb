cdc300dde4aa6ce82dc4a18fefc809dce035ee6c
澤なつみ <n2natsu1218@gmail.com>
2016-12-14 19:33:40 +0900

    make form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,18 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
+    @events = Event.all.order("id DESC")
   end
 
   def new
   end
 
   def create
+    Event.create(event_params)
+  end
+
+  private
+  def event_params
+    params.permit(:title, :time, :place, :detail)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'events'=> 'events#index'
   get 'events/new' => 'events#new'
+  post 'events' => 'events#create'
   root :to => 'top#index'
 end

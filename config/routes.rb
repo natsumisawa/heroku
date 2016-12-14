28267f833eb8302b2d440a2feaf941767c950452
澤なつみ <n2natsu1218@gmail.com>
2016-12-15 01:24:25 +0900

    no message

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'events'=> 'events#index'
-  get 'events/new' => 'events#new'
-  post 'events' => 'events#create'
-  root :to => 'top#index'
+  root 'top#index'
+  resources :events do
+    resources :reserves
+  end
 end
